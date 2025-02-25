4f415005c6d19ebb336b81e163050e8bf9287199
nrutachoudhari <nruta97@gmail.com>
2025-02-25 01:13:02 -0500

    initial commit

---
+++ backend/api/views/ProductView.py
@@ -0,0 +1,134 @@
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.authetication import SessionAuthentication, TokenAuthentication
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from django.db.utils import IntegrityError
+from socialmedia.models import Movie
+import logging
+
+# logging setup
+log_file = "./mlapi_logs.log"
+logging.basicConfig(
+    filename = log_file,
+    level = logging.INFO,
+    format = "%(asctime)s - %(levelname)s - %(message)s",
+)
+
+class Genres(APIView):
+    """
+    API View to handle CRUD operations for Genre objects
+    """
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request): 
+        try:
+            genre_id = request.query_params.get("id")
+            if not genre_id:
+                return Response(
+                    {"error": "1", "message": "Genre ID is required"}, status= 400
+                )
+            
+            genre = get_object_or_404(Genres, id = genre_id)
+
+            genre_data = {
+                "id": genre.id,
+                "name": genre.name,
+                "createdAt": genre.createdAt,
+            }
+            return Response(
+                {"error": "0", "message": "Success", "data": genre_data}, status = 200
+            )
+        
+        except Exception as e:
+            logging.error("Error fetching Movie record: %s", e)
+            return Response({"error": "2", "message": str(e)}, status = 500)
+        
+    def post(self, request):
+        """
+        Creating a new Genre record
+        """
+        try:
+            data = request.data
+            genreid = data.get("genreid")
+            name = data.get("name")
+            createdAt = data.get("createdAt", None)
+
+            if not genreid:
+                return Response(
+                    {"error": "1", "message": "Genre ID is required"},
+                    status = 400,
+                )
+            
+            genre = Genres.objects.create(
+                genreid = genreid,
+                name = name,
+                createdAt = createdAt,
+            )
+
+            return Response(
+                {"error": "0", "message": f"Genre {genre.name} created, status = 201"}
+            )
+        
+        except IntegrityError:
+            return Response(
+                {"error": "2", "message": "Genre record already exists"}, status = 400
+            )
+        
+        except Exception as e:
+            logging.error("Error creating Genre record %s", e)
+            return Response({"error": "3", "message": str(e)}, status=500)
+        
+    def put(self, request):
+        """
+        Update an existing Genre record.
+        """
+        try:
+            data = request.data
+            genre_id = data.get("id")
+
+            if not genre_id:
+                return Response(
+                    {"error": "1", "message": "Genre ID is required"}, status = 400
+                )
+            
+            genre = get_object_or_404(Genres, id = genre_id)
+
+            genre.id = data.get("id", genre.genreid)
+            genre.name = data.get("name", genre.name)
+            genre.createdAt = data.get("createdAt", genre.createdAt)
+
+            genre.save()
+
+            return Response(
+                {"error": "0", "message": f"Genre {genre.name} updated"}, status = 200
+            )
+        
+        except Exception as e:
+            logging.error("Error updating Genre record: %s", e)
+            return Response({"error": "3", "message": str(e)}, status=500)
+        
+    def delete(self, request):
+        """
+        delete a genre record
+        """
+        try:
+            genre_id = request.query_params.get("id")
+
+            if not genre_id:
+                return Response(
+                    {"error": "1", "message": "Genre ID is required"}, status = 400
+                )
+            
+            genre = get_object_or_404(Genres, id = genre_id)
+            genre.delete()
+
+            return Response(
+                {"error": "0", "message": f"Genre {genre.name} deleted"}, status=200
+            )
+
+        except Exception as e:
+            logging.error("Error deleting Genre record: %s, e")
+            return Response({"error": "3", "message": str(e)}, status=500)
